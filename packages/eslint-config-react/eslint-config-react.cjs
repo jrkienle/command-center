a9f766014736c7971d88703ee3bf522a4d4cf77b
James Kienle <james.kienle@gmail.com>
2023-03-11 16:44:04 -0800

    Add ESLint Configurations

---
+++ packages/eslint-config-react/eslint-config-react.cjs
@@ -0,0 +1,44 @@
+const base = require('./eslint-base.cjs');
+
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+
+  plugins: ['@typescript-eslint', 'tailwindcss'],
+  rules: {
+    // This allows us to import devdeps in tests and stories
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.stories.tsx', '**/*.test.ts', '**/*.test.tsx'],
+      },
+    ],
+    // This allows us to "export { default } from which is nice for reducing boilerplate"
+    'no-restricted-exports': 'off',
+    // This makes react-aria much more usable
+    'react/jsx-props-no-spreading': 'off',
+    // We use React 18, this is no longer needed
+    'react/react-in-jsx-scope': 'off',
+    // This rule is broken in TypeScript
+    'react/require-default-props': 'off',
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['clsx', 'twsx'],
+      config: './node_modules/@antribute/zephyr-core/tailwind.config.cjs',
+      removeDuplicates: true,
+    },
+  },
+};
